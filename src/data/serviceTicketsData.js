bbea3368ebc229adcc4f380badcb845bb0178010
tderamus <deramust@gmail.com>
2025-02-03 22:08:03 -0600

    added api calles to frontend to pull backend data

--- src/data/serviceTicketsData.js
+++ src/data/serviceTicketsData.js
@@ -1,7 +1,27 @@
 const _apiUrl = "/api/servicetickets";
 
-export const getServiceTickets = () => {
-  return fetch(_apiUrl).then((r) => r.json());
+export const getServiceTickets = async () => {
+  const response = await fetch("http://localhost:5001/api/serviceTickets/");
+  if (!response.ok) {
+    throw new Error("Failed to fetch service tickets");
+  }
+  return response.json();
 };
 
+
+
 //export a function here that gets a ticket by id
+export async function getServiceTicketsById(id) {
+  try {
+    const response = await fetch(`/api/servicetickets/${id}`);
+
+    if(!response.ok){
+      throw new Error(`Failed to fetch service ticket with ID ${id}`)
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching service ticket:", error);
+    return null;
+  }
+ 
+} 
